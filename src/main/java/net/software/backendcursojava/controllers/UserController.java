94e6e7be5b3c11df906b9aa96d9a0869b6d44c8c
Juan Caicedo <jscp2619@gmail.com>
2021-09-25 18:30:24 -0500

    Update code with entities

--- src/main/java/net/software/backendcursojava/controllers/UserController.java
+++ src/main/java/net/software/backendcursojava/controllers/UserController.java
@@ -1,5 +1,7 @@
 package net.software.backendcursojava.controllers;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,12 +9,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
 import net.software.backendcursojava.models.request.UserDetailRequestModel;
 import net.software.backendcursojava.models.responses.UserRest;
+import net.software.backendcursojava.services.UserServiceInterface;
+import net.software.backendcursojava.shared.dto.UserDTO;
 
 @RestController
 @RequestMapping("/users") //localhost/users
 public class UserController{
+
+    @Autowired
+    UserServiceInterface userService;
+    
 
     @GetMapping //Obtener, consultar informacion
     public String getUser(){
@@ -21,7 +30,17 @@ public class UserController{
 
     @PostMapping //Creando informacion
     public UserRest createUser(@RequestBody UserDetailRequestModel userDetails){
-        return null;
+        UserRest UserToReturn= new UserRest();
+
+        UserDTO userDTO =new UserDTO();
+
+        BeanUtils.copyProperties(userDetails, userDTO);
+
+        UserDTO createdUser= userService.createUser(userDTO);
+
+        BeanUtils.copyProperties(createdUser, UserToReturn);
+
+        return UserToReturn;
     }
 
 }
---
+++ src/main/java/net/software/backendcursojava/services/UserService.java
@@ -0,0 +1,15 @@
+package net.software.backendcursojava.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements  UserServiceInterface{
+
+    @Override
+    public UserDTO createUser(UserDto userDTO) {
+        // TODO lógica de la creación de usuario.
+        return null;
+    }
+
+    
+}
