0c45d42f38e4e7c3b802d49340a917d23e2510e1
Juan Caicedo <jscp2619@gmail.com>
2021-10-04 21:22:08 -0500

    Actualización de dependecias y excepción del Email

--- src/main/java/net/software/backendcursojava/services/UserService.java
+++ src/main/java/net/software/backendcursojava/services/UserService.java
@@ -1,5 +1,7 @@
 package net.software.backendcursojava.services;
 
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,12 +20,16 @@ public class UserService implements  UserServiceInterface{
     @Override
     public UserDTO createUser(UserDTO userDTO) {
         
-        
+        if(userRepository.findUserByEmail(userDTO.getEmail()) != null) throw new RuntimeException("Su correo ya existe.");
+
+
         UserEntity userEntity =new UserEntity();
         BeanUtils.copyProperties(userDTO, userEntity);
 
         userEntity.setEncryptedPassword("testpassword");
-        userEntity.setUserId("testUserId");
+        UUID userId= UUID.randomUUID();
+        userEntity.setUserId(userId.toString());
+        
 
 
         UserEntity storedUserDetail= userRepository.save(userEntity);
